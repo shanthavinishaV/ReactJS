de57fdc1b04134c58c0139f85d84e0d24b5c88b4
shanthavinishaV <168687295+shanthavinishaV@users.noreply.github.com>
2024-05-08 11:39:38 +0530

    Update forEachMovies.js

--- ReactJS_Assignments/Day3_Assignment/forEachMovies.js
+++ ReactJS_Assignments/Day3_Assignment/forEachMovies.js
@@ -1,5 +1,3 @@
-//Iterate with `forEach`
-
 const oscarMovies = [
     { title: "Parasite", year: 2020, genre: "Thriller", director: "Bong Joon Ho" },
     { title: "Green Book", year: 2019, genre: "Drama", director: "Peter Farrelly" },
@@ -8,13 +6,13 @@ const oscarMovies = [
     { title: "Avengers End Game", year: 2019, genre: "Adventure", director: "Joe and Anthony Russo" }
     ];
 
+    //Iterate with `forEach`
     oscarMovies.forEach(function(movies,index,array){
-        console.log("Index:" + index + "," + "Title:" + "," + movies.title)
-        console.log(array);
+        console.log(`"Index: ${index}, Title: ${movies.title}, Movie: ${JSON.stringify(array)}"`);
         });
      
 //Iterate with a `for` loop
 for (var i = 0; i < oscarMovies.length; i++) {
     console.log("Iterate with a `for` loop");
-    console.log(oscarMovies[i]);
+    console.log(`"Index: ${i}, Title: ${oscarMovies[i].title}, Movie: "${JSON.stringify(oscarMovies[i])}"`);
 }
